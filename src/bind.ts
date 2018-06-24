7b71445bcd2055b30f6d996bc90cb37b34f2c8b5
Danko Aleksejevs <danko@very.lv>
2018-06-24 23:01:58 +0300

    Support custom "components"

--- src/bind.ts
+++ src/bind.ts
@@ -6,18 +6,28 @@ import { extractDOMContext, UJSXDOMContextParam } from './dom/context';
 import { createUJSXObject } from './common/object';
 
 export function ujsxFor(ctx: UJSXDOMContextParam) {
+
 	const ectx = extractDOMContext(ctx);
-	const ujsx = ectx.window
+
+	const createElement = ectx.window
 		? (tag: string, attributes: { [key: string]: any }, ...children: any[]) => createDOMElement(ectx, tag, attributes, ...children)
 		: (tag: string, attributes: { [key: string]: any }, ...children: any[]) => createUJSXObject(tag, attributes, ...children);
+
 	function ujsxToDOMBound(ujsx: UJSXArray): Node[];
 	function ujsxToDOMBound(ujsx: UJSX): Node;
 	function ujsxToDOMBound(ujsx: UJSXArray | UJSX): Node[] | Node {
 		return ujsxToDOM(ectx, ujsx);
 	}
+
+	function ujsx(tag: string | ((props: any) => UJSX), attributes: { [key: string]: any }, ...children: any[]): UJSX {
+		if (typeof tag == 'function') return tag(Object.assign({}, attributes, { children }));
+		return createElement(tag, attributes, ...children);
+	}
+
 	return {
 		ujsx,
 		ujsxToHTML: (ujsx: UJSX) => ujsxToHTML(ectx, ujsx),
 		ujsxToDOM: ujsxToDOMBound
 	};
+
 }
