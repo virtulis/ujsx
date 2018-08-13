4003c47db1d8f73e2fb255da085826ccc170cafc
Danko Aleksejevs <danko@very.lv>
2018-08-14 00:26:46 +0300

    Allow null as UJSX param

--- src/common/types.ts
+++ src/common/types.ts
@@ -2,7 +2,7 @@ import { UJSXObject } from './object';
 
 export interface UJSXArray extends Array<UJSX> {}
 
-export type UJSXNonArray = UJSXObject | string | Element | JSX.Element;
+export type UJSXNonArray = UJSXObject | string | Element | JSX.Element | null;
 export type UJSX = UJSXNonArray | UJSXArray;
 
 export type UJSXHandler = (tag: string, attributes: { [key: string]: any }, ...children: UJSX[]) => UJSX;
