faaf2248206ea2c06a8470da08ed76390b707062
Danko Aleksejevs <danko@very.lv>
2019-04-01 00:48:32 +0300

    Bump packages, fix JSX.Element override

--- src/common/global.ts
+++ src/common/global.ts
@@ -1,5 +1,5 @@
 // TypeScript assumes JSX code returns "JSX.Element" so we hack it a little to allow mingling it with DOM
 type DOMElement = Element;
 declare namespace JSX {
-	interface Element extends DOMElement, React.ReactElement<any> { }
+	interface Element extends DOMElement, React.ReactElement<any, any> { }
 }
